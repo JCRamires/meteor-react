ca68ffaf3db0a2a29cb31777a3585da1b47d4e6c
Juan Carlos Ramires <jcramires851@gmail.com>
2016-04-28 16:18:00 -0300

    refactor ViewEntidades

--- imports/ui/ViewEntidades.jsx
+++ imports/ui/ViewEntidades.jsx
@@ -6,24 +6,41 @@ import { createContainer } from 'meteor/react-meteor-data'
 
 import {Entidades} from '../api/collections.js'
 
-import EntidadeRow from '../ui/EntidadeRow.jsx'
-
 export default class ViewEntidade extends Component {
     renderEntidades(){
         return this.props.entidades.map((entidade) => (
-            <EntidadeRow key={entidade._id} nome={entidade.nome} endereco={entidade.endereco} tipo={entidade.tipo} documento={entidade.documento} />
+            <tr key='entidade._id'>
+                <td>
+                    {entidade.nome}
+                </td>
+                <td>
+                    {entidade.endereco}
+                </td>
+                <td>
+                    {entidade.tipo}
+                </td>
+                <td>
+                    {entidade.documento}
+                </td>
+            </tr>
         ))
     }
 
     render(){
         return (
-            <div className='ui grid'>
-                <div className='row'>
-                    <div className='column'>
-                        {this.renderEntidades()}
-                    </div>
-                </div>
-            </div>
+            <table className='ui celled table'>
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Endereço</th>
+                        <th>Tipo</th>
+                        <th>Documento</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.renderEntidades()}
+                </tbody>
+            </table>
         )
     }
 }
